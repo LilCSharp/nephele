8a17e2106d0dbf87a6bd84bf2f1b7b328c6783f8
LilCSharp <caleb.li.wheeler@gmail.com>
2024-02-09 23:38:36 -0500

    Implemented board and div motion

--- src/DiagramContainer.tsx
+++ src/DiagramContainer.tsx
@@ -2,8 +2,10 @@ import React, { useState, MouseEvent } from "react";
 import Rectangle from "./Rectangle";
 
 const Diagram: React.FC = () => {
-    const [rectangles, setRectangles] = useState<Rectangle[]>([new Rectangle(50, 50, 100, 100)]);
+    const [rectangles, setRectangles] = useState<Rectangle[]>([new Rectangle(50, 50, 100, 100), new Rectangle(200, 200, 100, 100)]);
     const [selection, setSelection] = useState<number | null>(null);
+    const [initialX, setInitialX] = useState<number>(0);
+    const [initialY, setInitialY] = useState<number>(0);
 
     const selectBoard = () => {
         setSelection(-1);
@@ -14,6 +16,46 @@ const Diagram: React.FC = () => {
         e.stopPropagation();
         setSelection(index);
         console.log("Rect selected");
+    }
+
+    const translateElement = (e: MouseEvent<HTMLDivElement>) => {
+        const deltaX = e.clientX - initialX;
+        const deltaY = e.clientY - initialY;
+
+        if (selection === -1) {
+            const updatedRectangles = rectangles.map(rectangle => {
+                return new Rectangle(
+                    rectangle.x + deltaX,
+                    rectangle.y + deltaY,
+                    rectangle.width,
+                    rectangle.height
+                );
+            });
+
+            setRectangles(updatedRectangles);
+        } else if (selection != null) {
+            const updatedRectangles = rectangles.map((rectangle, index) => {
+                if (selection === index) {
+                    return new Rectangle(
+                        rectangle.x + deltaX,
+                        rectangle.y + deltaY,
+                        rectangle.width,
+                        rectangle.height
+                    )
+                }
+
+                return rectangle;
+            });
+
+            setRectangles(updatedRectangles);
+        }
+
+        setInitialX(e.clientX);
+        setInitialY(e.clientY);
+    }
+
+    const endSelection = () => {
+        setSelection(null);
     }
 
     return (
@@ -29,6 +71,8 @@ const Diagram: React.FC = () => {
                 display: 'flex'
             }}
             onMouseDown={selectBoard}
+            onMouseMove={translateElement}
+            onMouseUp={endSelection}
         >
             {rectangles.map((rectangle, index) => (
                 <React.Fragment key={index}>{rectangle.getRectangleComponent(index, selectElement)}</React.Fragment>
