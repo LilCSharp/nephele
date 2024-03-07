5d6ca3b6a8d5f7b5eb425e9d3319672d5c3a203f
LilCSharp <caleb.li.wheeler@gmail.com>
2024-03-07 18:31:18 -0500

    Added a rudimentary navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,8 +2,21 @@ import React, { CSSProperties } from "react";
 import "./Navbar.css";
 
 const NavBar: React.FC = () => {
+    const renderGridSquares = (count: number) => {
+        const squares = [];
+
+        for (let i = 0; i < count; i++) {
+            squares.push(<div key={i} className="sidebar-grid-item"></div>);
+        }
+
+        return squares
+    }
+
     return (
         <div className="sidebar">
+            <div className="sidebar-grid-container">
+                {renderGridSquares(20)}
+            </div>
         </div>
     );
 }
